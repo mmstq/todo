52ae8456871b870ee1ad56791f5f7631fe9305b4
mmstq <mushtakkhan9@gmail.com>
2024-03-08 20:49:38 +0530

    first

---
+++ lib/gen/colors.gen.dart
@@ -0,0 +1,375 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class ColorName {
+  ColorName._();
+
+  /// Color: #000000
+  static const Color black = Color(0xFF000000);
+
+  /// Color: #0030CC
+  static const Color blue1 = Color(0xFF0030CC);
+
+  /// Color: #B3C5FF
+  static const Color blue10 = Color(0xFFB3C5FF);
+
+  /// Color: #CCD8FF
+  static const Color blue11 = Color(0xFFCCD8FF);
+
+  /// Color: #E6ECFF
+  static const Color blue12 = Color(0xFFE6ECFF);
+
+  /// Color: #0036E6
+  static const Color blue2 = Color(0xFF0036E6);
+
+  /// Color: #003CFF
+  static const Color blue3 = Color(0xFF003CFF);
+
+  /// Color: #1A50FF
+  static const Color blue4 = Color(0xFF1A50FF);
+
+  /// Color: #3363FF
+  static const Color blue5 = Color(0xFF3363FF);
+
+  /// Color: #4D77FF
+  static const Color blue6 = Color(0xFF4D77FF);
+
+  /// Color: #668AFF
+  static const Color blue7 = Color(0xFF668AFF);
+
+  /// Color: #809EFF
+  static const Color blue8 = Color(0xFF809EFF);
+
+  /// Color: #99B1FF
+  static const Color blue9 = Color(0xFF99B1FF);
+
+  /// Color: #B99750
+  static const Color golden1 = Color(0xFFB99750);
+
+  /// Color: #F1EADC
+  static const Color golden11 = Color(0xFFF1EADC);
+
+  /// Color: #F8F5EE
+  static const Color golden12 = Color(0xFFF8F5EE);
+
+  /// Color: #C0A262
+  static const Color golden2 = Color(0xFFC0A262);
+
+  /// Color: #C7AC73
+  static const Color golden3 = Color(0xFFC7AC73);
+
+  /// Color: #CBB17C
+  static const Color golden4 = Color(0xFFCBB17C);
+
+  /// Color: #CEB785
+  static const Color golden5 = Color(0xFFCEB785);
+
+  /// Color: #D5C196
+  static const Color golden6 = Color(0xFFD5C196);
+
+  /// Color: #DBCAA7
+  static const Color golden7 = Color(0xFFDBCAA7);
+
+  /// Color: #E3D5B9
+  static const Color golden8 = Color(0xFFE3D5B9);
+
+  /// Color: #E7DBC2
+  static const Color golden9 = Color(0xFFE7DBC2);
+
+  /// Color: #0F0D15
+  static const Color gray1 = Color(0xFF0F0D15);
+
+  /// Color: #D2D1D4
+  static const Color gray10 = Color(0xFFD2D1D4);
+
+  /// Color: #E8E8EA
+  static const Color gray11 = Color(0xFFE8E8EA);
+
+  /// Color: #F9F9F9
+  static const Color gray12 = Color(0xFFF9F9F9);
+
+  /// Color: #1D1929
+  static const Color gray2 = Color(0xFF1D1929);
+
+  /// Color: #34303E
+  static const Color gray3 = Color(0xFF34303E);
+
+  /// Color: #4A4754
+  static const Color gray4 = Color(0xFF4A4754);
+
+  /// Color: #615E69
+  static const Color gray5 = Color(0xFF615E69);
+
+  /// Color: #77757F
+  static const Color gray6 = Color(0xFF77757F);
+
+  /// Color: #8E8C94
+  static const Color gray7 = Color(0xFF8E8C94);
+
+  /// Color: #A5A3A9
+  static const Color gray8 = Color(0xFFA5A3A9);
+
+  /// Color: #BBBABF
+  static const Color gray9 = Color(0xFFBBBABF);
+
+  /// Color: #18BA92
+  static const Color green1 = Color(0xFF18BA92);
+
+  /// Color: #BCF8E9
+  static const Color green10 = Color(0xFFBCF8E9);
+
+  /// Color: #D2FBF0
+  static const Color green11 = Color(0xFFD2FBF0);
+
+  /// Color: #E9FDF8
+  static const Color green12 = Color(0xFFE9FDF8);
+
+  /// Color: #1BD2A4
+  static const Color green2 = Color(0xFF1BD2A4);
+
+  /// Color: #1EE9B6
+  static const Color green3 = Color(0xFF1EE9B6);
+
+  /// Color: #35EBBD
+  static const Color green4 = Color(0xFF35EBBD);
+
+  /// Color: #4BEDC5
+  static const Color green5 = Color(0xFF4BEDC5);
+
+  /// Color: #62F0CC
+  static const Color green6 = Color(0xFF62F0CC);
+
+  /// Color: #78F2D3
+  static const Color green7 = Color(0xFF78F2D3);
+
+  /// Color: #8FF4DB
+  static const Color green8 = Color(0xFF8FF4DB);
+
+  /// Color: #A5F6E2
+  static const Color green9 = Color(0xFFA5F6E2);
+
+  /// Color: #47143D
+  static const Color indigo1 = Color(0xFF47143D);
+
+  /// Color: #E4DCE2
+  static const Color indigo10 = Color(0xFFE4DCE2);
+
+  /// Color: #EDE8EC
+  static const Color indigo11 = Color(0xFFEDE8EC);
+
+  /// Color: #F6F3F5
+  static const Color indigo12 = Color(0xFFF6F3F5);
+
+  /// Color: #5A2C51
+  static const Color indigo2 = Color(0xFF5A2C51);
+
+  /// Color: #6C4364
+  static const Color indigo3 = Color(0xFF6C4364);
+
+  /// Color: #7F5B78
+  static const Color indigo4 = Color(0xFF7F5B78);
+
+  /// Color: #91728B
+  static const Color indigo5 = Color(0xFF91728B);
+
+  /// Color: #A2899D
+  static const Color indigo6 = Color(0xFFA2899D);
+
+  /// Color: #B5A1B1
+  static const Color indigo7 = Color(0xFFB5A1B1);
+
+  /// Color: #C8B9C5
+  static const Color indigo8 = Color(0xFFC8B9C5);
+
+  /// Color: #DAD0D8
+  static const Color indigo9 = Color(0xFFDAD0D8);
+
+  /// Color: #CC5801
+  static const Color orange1 = Color(0xFFCC5801);
+
+  /// Color: #FFD4B3
+  static const Color orange10 = Color(0xFFFFD4B3);
+
+  /// Color: #FFE2CC
+  static const Color orange11 = Color(0xFFFFE2CC);
+
+  /// Color: #FFF1E6
+  static const Color orange12 = Color(0xFFFFF1E6);
+
+  /// Color: #E66301
+  static const Color orange2 = Color(0xFFE66301);
+
+  /// Color: #FF6E01
+  static const Color orange3 = Color(0xFFFF6E01);
+
+  /// Color: #FF7D1A
+  static const Color orange4 = Color(0xFFFF7D1A);
+
+  /// Color: #FF8B34
+  static const Color orange5 = Color(0xFFFF8B34);
+
+  /// Color: #FF9A4D
+  static const Color orange6 = Color(0xFFFF9A4D);
+
+  /// Color: #FFA867
+  static const Color orange7 = Color(0xFFFFA867);
+
+  /// Color: #FFB780
+  static const Color orange8 = Color(0xFFFFB780);
+
+  /// Color: #FFC599
+  static const Color orange9 = Color(0xFFFFC599);
+
+  /// Color: #C80068
+  static const Color pink1 = Color(0xFFC80068);
+
+  /// Color: #FEB3DA
+  static const Color pink10 = Color(0xFFFEB3DA);
+
+  /// Color: #FECCE6
+  static const Color pink11 = Color(0xFFFECCE6);
+
+  /// Color: #FFE6F3
+  static const Color pink12 = Color(0xFFFFE6F3);
+
+  /// Color: #E10075
+  static const Color pink2 = Color(0xFFE10075);
+
+  /// Color: #FA0082
+  static const Color pink3 = Color(0xFFFA0082);
+
+  /// Color: #FB1A8F
+  static const Color pink4 = Color(0xFFFB1A8F);
+
+  /// Color: #FB339B
+  static const Color pink5 = Color(0xFFFB339B);
+
+  /// Color: #FC4DA8
+  static const Color pink6 = Color(0xFFFC4DA8);
+
+  /// Color: #FC66B4
+  static const Color pink7 = Color(0xFFFC66B4);
+
+  /// Color: #FD80C1
+  static const Color pink8 = Color(0xFFFD80C1);
+
+  /// Color: #FD99CD
+  static const Color pink9 = Color(0xFFFD99CD);
+
+  /// Color: #AE59C6
+  static const Color purple1 = Color(0xFFAE59C6);
+
+  /// Color: #F4D4FD
+  static const Color purple10 = Color(0xFFF4D4FD);
+
+  /// Color: #F7E2FE
+  static const Color purple11 = Color(0xFFF7E2FE);
+
+  /// Color: #FBF1FE
+  static const Color purple12 = Color(0xFFFBF1FE);
+
+  /// Color: #C364DF
+  static const Color purple2 = Color(0xFFC364DF);
+
+  /// Color: #D96FF8
+  static const Color purple3 = Color(0xFFD96FF8);
+
+  /// Color: #DD7DF9
+  static const Color purple4 = Color(0xFFDD7DF9);
+
+  /// Color: #E18CF9
+  static const Color purple5 = Color(0xFFE18CF9);
+
+  /// Color: #E49AFA
+  static const Color purple6 = Color(0xFFE49AFA);
+
+  /// Color: #E8A9FB
+  static const Color purple7 = Color(0xFFE8A9FB);
+
+  /// Color: #ECB7FC
+  static const Color purple8 = Color(0xFFECB7FC);
+
+  /// Color: #F0C5FC
+  static const Color purple9 = Color(0xFFF0C5FC);
+
+  /// Color: #C32021
+  static const Color red1 = Color(0xFFC32021);
+
+  /// Color: #FCBFBF
+  static const Color red10 = Color(0xFFFCBFBF);
+
+  /// Color: #FDD4D4
+  static const Color red11 = Color(0xFFFDD4D4);
+
+  /// Color: #FEEAEA
+  static const Color red12 = Color(0xFFFEEAEA);
+
+  /// Color: #DC2425
+  static const Color red2 = Color(0xFFDC2425);
+
+  /// Color: #F42829
+  static const Color red3 = Color(0xFFF42829);
+
+  /// Color: #F53E3E
+  static const Color red4 = Color(0xFFF53E3E);
+
+  /// Color: #F65354
+  static const Color red5 = Color(0xFFF65354);
+
+  /// Color: #F76969
+  static const Color red6 = Color(0xFFF76969);
+
+  /// Color: #F87E7F
+  static const Color red8 = Color(0xFFF87E7F);
+
+  /// Color: #FBA9A9
+  static const Color red9 = Color(0xFFFBA9A9);
+
+  /// Color: #FFFFFF
+  static const Color white = Color(0xFFFFFFFF);
+
+  /// Color: #CCB243
+  static const Color yellow1 = Color(0xFFCCB243);
+
+  /// Color: #FFF5CC
+  static const Color yellow10 = Color(0xFFFFF5CC);
+
+  /// Color: #FFF8DD
+  static const Color yellow11 = Color(0xFFFFF8DD);
+
+  /// Color: #FFFCEE
+  static const Color yellow12 = Color(0xFFFFFCEE);
+
+  /// Color: #E6C84C
+  static const Color yellow2 = Color(0xFFE6C84C);
+
+  /// Color: #FFDE54
+  static const Color yellow3 = Color(0xFFFFDE54);
+
+  /// Color: #FFE165
+  static const Color yellow4 = Color(0xFFFFE165);
+
+  /// Color: #FFE576
+  static const Color yellow5 = Color(0xFFFFE576);
+
+  /// Color: #FFE887
+  static const Color yellow6 = Color(0xFFFFE887);
+
+  /// Color: #FFEB98
+  static const Color yellow7 = Color(0xFFFFEB98);
+
+  /// Color: #FFEFAA
+  static const Color yellow8 = Color(0xFFFFEFAA);
+
+  /// Color: #FFF2BB
+  static const Color yellow9 = Color(0xFFFFF2BB);
+}
